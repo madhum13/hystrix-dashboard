71636f5ef20fb3bd9cb97cee65b5535da86c8886
madhum13 <madhumpura.1998@gmail.com>
2022-06-15 20:13:54 +0530

    initial commit

---
+++ src/test/java/com/mycompany/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
